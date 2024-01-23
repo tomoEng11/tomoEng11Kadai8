77baf52cef58fba6f11114ff7ef396efcedc64b3
tomo <tom_friendship@me.com>
2024-01-23 22:27:39 +0900

    initial commit

--- tomoEng11Kadai8/AppDelegate.swift
+++ tomoEng11Kadai8/AppDelegate.swift
@@ -10,6 +10,8 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var sharedSliderValue: Float = 0.0
+
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
---
+++ tomoEng11Kadai8/RedViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  tomoEng11Kadai8
+//
+//  Created by 井本智博 on 2024/01/23.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    private let delegate = UIApplication.shared.delegate as! AppDelegate
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        slider.value = delegate.sharedSliderValue
+        label.text = String(slider.value)
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        delegate.sharedSliderValue = slider.value
+        label.text = String(slider.value)
+    }
+    
+
+}
+
